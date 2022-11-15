fef32ab2f92f018057f60a15eff0fb8ba80d40bb
John-Paul Navarro <navarro@anl.gov>
2022-11-15 14:11:00 -0600

    See CHANGELOG v3.1.0

--- Operations_Warehouse_Django/cider/serializers.py
+++ Operations_Warehouse_Django/cider/serializers.py
@@ -17,60 +17,197 @@ class CiderInfrastructure_Summary_Serializer(serializers.ModelSerializer):
             return CiderInfrastructure.other_attributes['public_url']
         except:
             return None
-    def get_cider_data_url(self, CiderInfrastructure):
+    def get_cider_data_url(self, object):
         http_request = self.context.get('request')
         if http_request:
-            return http_request.build_absolute_uri(uri_to_iri(reverse('cider-detail-v1-id', args=[CiderInfrastructure.cider_resource_id])))
+            return http_request.build_absolute_uri(uri_to_iri(reverse('cider-detail-v1-id', args=[object.cider_resource_id])))
         else:
             return ''
-    def get_organization_name(self, CiderInfrastructure):
+    def get_organization_name(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_name']
+            return object.other_attributes['organizations'][0]['organization_name']
         except:
             return None
-    def get_organization_url(self, CiderInfrastructure):
+    def get_organization_url(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_url']
+            return object.other_attributes['organizations'][0]['organization_url']
         except:
             return None
-    def get_organization_logo_url(self, CiderInfrastructure):
+    def get_organization_logo_url(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_logo_url']
+            return object.other_attributes['organizations'][0]['organization_logo_url']
+        except:
+            return None
+
+class CiderInfrastructure_Compute_Serializer(serializers.ModelSerializer):
+    user_guide_url = serializers.SerializerMethodField()
+    cpu_type = serializers.SerializerMethodField()
+    operating_system = serializers.SerializerMethodField()
+    gpu_description = serializers.SerializerMethodField()
+    job_manager = serializers.SerializerMethodField()
+    batch_system = serializers.SerializerMethodField()
+    interconnect = serializers.SerializerMethodField()
+    storage_network = serializers.SerializerMethodField()
+    machine_type = serializers.SerializerMethodField()
+    manufacturer = serializers.SerializerMethodField()
+    cpu_speed_ghz = serializers.SerializerMethodField()
+    platform_name = serializers.SerializerMethodField()
+    memory_per_cpu_gb = serializers.SerializerMethodField()
+    cpu_count_per_node = serializers.SerializerMethodField()
+    xsedenet_participation = serializers.SerializerMethodField()
+    supports_senstitive_data = serializers.SerializerMethodField()
+    local_storage_per_node_gb = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = CiderInfrastructure
+        fields = ('cider_resource_id', 'recommended_use', 'access_description', 'user_guide_url', 'cpu_type', 'operating_system', 'gpu_description', 'job_manager', 'batch_system', 'interconnect', 'storage_network', 'machine_type', 'manufacturer', 'cpu_speed_ghz', 'platform_name', 'memory_per_cpu_gb', 'cpu_count_per_node', 'xsedenet_participation', 'supports_senstitive_data', 'local_storage_per_node_gb')
+
+    def get_user_guide_url(self, object):
+        try:
+            return object.other_attributes['user_guide_url']
+        except:
+            return None
+            
+    def get_cpu_type(self, object):
+        try:
+            return object.other_attributes['cpu_type']
+        except:
+            return None
+            
+    def get_operating_system(self, object):
+        try:
+            return object.other_attributes['operating_system']
+        except:
+            return None
+
+    def get_gpu_description(self, object):
+        try:
+            return object.other_attributes['gpu_description']
+        except:
+            return None
+            
+    def get_job_manager(self, object):
+        try:
+            return object.other_attributes['job_manager']
+        except:
+            return None
+            
+    def get_batch_system(self, object):
+        try:
+            return object.other_attributes['batch_system']
+        except:
+            return None
+            
+    def get_interconnect(self, object):
+        try:
+            return object.other_attributes['interconnect']
+        except:
+            return None
+            
+    def get_storage_network(self, object):
+        try:
+            return object.other_attributes['storage_network']
+        except:
+            return None
+            
+    def get_machine_type(self, object):
+        try:
+            return object.other_attributes['machine_type']
+        except:
+            return None
+            
+    def get_manufacturer(self, object):
+        try:
+            return object.other_attributes['manufacturer']
+        except:
+            return None
+            
+    def get_cpu_speed_ghz(self, object):
+        try:
+            return object.other_attributes['cpu_speed_ghz']
+        except:
+            return None
+            
+    def get_platform_name(self, object):
+        try:
+            return object.other_attributes['platform_name']
+        except:
+            return None
+            
+    def get_memory_per_cpu_gb(self, object):
+        try:
+            return object.other_attributes['memory_per_cpu_gb']
+        except:
+            return None
+            
+    def get_cpu_count_per_node(self, object):
+        try:
+            return object.other_attributes['cpu_count_per_node']
+        except:
+            return None
+            
+    def get_xsedenet_participation(self, object):
+        try:
+            return object.other_attributes['xsedenet_participation']
+        except:
+            return None
+            
+    def get_supports_senstitive_data(self, object):
+        try:
+            return object.other_attributes['supports_senstitive_data']
+        except:
+            return None
+            
+    def get_local_storage_per_node_gb(self, object):
+        try:
+            return object.other_attributes['local_storage_per_node_gb']
         except:
             return None
 
 class CiderInfrastructure_Detail_Serializer(serializers.ModelSerializer):
+    cider_type = serializers.SerializerMethodField()
     cider_view_url = serializers.SerializerMethodField()
     _result_item_url = serializers.SerializerMethodField()
     organization_name = serializers.SerializerMethodField()
     organization_url = serializers.SerializerMethodField()
     organization_logo_url = serializers.SerializerMethodField()
+    compute = serializers.SerializerMethodField()
+    
     class Meta:
         model = CiderInfrastructure
-        fields = ('__all__')
-    def get_cider_view_url(self, CiderInfrastructure):
+        exclude = ('parent_resource', 'recommended_use', 'access_description', 'provider_level')
+        
+    def get_cider_type(self, object):
+        return('compute')
+    def get_cider_view_url(self, object):
         try:
-            return CiderInfrastructure.other_attributes['public_url']
+            return object.other_attributes['public_url']
         except:
             return None
-    def get__result_item_url(self, CiderInfrastructure):
+    def get__result_item_url(self, object):
         http_request = self.context.get('request')
         if http_request:
-            return http_request.build_absolute_uri(uri_to_iri(reverse('cider-detail-v1-id', args=[CiderInfrastructure.cider_resource_id])))
+            return http_request.build_absolute_uri(uri_to_iri(reverse('cider-detail-v1-id', args=[object.cider_resource_id])))
         else:
             return ''
-    def get_organization_name(self, CiderInfrastructure):
+    def get_organization_name(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_name']
+            return object.other_attributes['organizations'][0]['organization_name']
         except:
             return None
-    def get_organization_url(self, CiderInfrastructure):
+    def get_organization_url(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_url']
+            return object.other_attributes['organizations'][0]['organization_url']
         except:
             return None
-    def get_organization_logo_url(self, CiderInfrastructure):
+    def get_organization_logo_url(self, object):
         try:
-            return CiderInfrastructure.other_attributes['organizations'][0]['organization_logo_url']
+            return object.other_attributes['organizations'][0]['organization_logo_url']
+        except:
+            return None
+    def get_compute(self, object):
+        try:
+            return CiderInfrastructure_Compute_Serializer(
+                CiderInfrastructure.objects.get(parent_resource=object.cider_resource_id, cider_type='compute')).data
         except:
             return None
--- Operations_Warehouse_Django/cider/views.py
+++ Operations_Warehouse_Django/cider/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -24,6 +25,17 @@ class CiderInfrastructure_v1_ACCESSActiveList(APIView):
         serializer = CiderInfrastructure_Summary_Serializer(objects, context={'request': request}, many=True)
         return MyAPIResponse({'results': serializer.data})
 
+class CiderInfrastructure_v1_ACCESSGatewayActiveList(APIView):
+    '''
+        All ACCESS Active Resources
+    '''
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    renderer_classes = (JSONRenderer,)
+    def get(self, request, format=None, **kwargs):
+        objects = CiderInfrastructure_Active(affiliation='XSEDE', allocated=True, type='BASE', result='OBJECTS')
+        serializer = CiderInfrastructure_Summary_Serializer(objects, context={'request': request}, many=True)
+        return MyAPIResponse({'results': serializer.data})
+
 class CiderInfrastructure_v1_Detail(APIView):
     '''
         An ACCESS Resource Detail
@@ -31,17 +43,20 @@ class CiderInfrastructure_v1_Detail(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     renderer_classes = (JSONRenderer,)
     def get(self, request, format=None, **kwargs):
-        if self.kwargs.get('cider_resource_id'):
+        # We need the base resource to pass to the serializer
+        if self.kwargs.get('cider_resource_id'):        # Whether base or sub resource, grab the other one also
             try:
-                objects = [CiderInfrastructure.objects.get(pk=self.kwargs['cider_resource_id'])]
+                item = CiderInfrastructure.objects.get(pk=self.kwargs['cider_resource_id'])
+                if item.parent_resource: # Serializer wants the parent, but will serialize both
+                    item =  CiderInfrastructure.objects.get(pk=item.parent_resource)
             except CiderInfrastructure.DoesNotExist:
                 raise MyAPIException(code=status.HTTP_400_BAD_REQUEST, detail='Specified cider_resource_id not found')
         elif self.kwargs.get('info_resourceid'):
             try:
-                objects = CiderInfrastructure.objects.filter(info_resourceid__exact=self.kwargs['info_resourceid'])
-            except CiderInfrastructure.DoesNotExist:
-                raise MyAPIException(code=status.HTTP_400_BAD_REQUEST, detail='Specified info_resourceid not found')
+                item = CiderInfrastructure.objects.get(info_resourceid=self.kwargs['info_resourceid'], cider_type='resource')
+            except (CiderInfrastructure.DoesNotExist, CiderInfrastructure.MultipleObjectsReturned):
+                raise MyAPIException(code=status.HTTP_400_BAD_REQUEST, detail='Specified info_resourceid did not match a single resource')
         else:
             raise MyAPIException(code=status.HTTP_400_BAD_REQUEST, detail='Missing selection parameter')
-        serializer = CiderInfrastructure_Detail_Serializer(objects, context={'request': request}, many=True)
+        serializer = CiderInfrastructure_Detail_Serializer(item, context={'request': request})
         return MyAPIResponse({'result_set': serializer.data})
