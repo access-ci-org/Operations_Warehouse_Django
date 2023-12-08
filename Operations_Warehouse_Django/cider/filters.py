7b04d37f1809480e4723627e5dd8fb62e2efd3c9
John-Paul Navarro <navarro@anl.gov>
2023-12-08 10:27:34 -0600

    See CHANGELOG tag v3.21.0

--- Operations_Warehouse_Django/cider/filters.py
+++ Operations_Warehouse_Django/cider/filters.py
@@ -36,42 +36,28 @@ from itertools import chain
 ###############################################################################
 
 active_statuses = ('friendly', 'coming soon', 'pre-production', 'production', 'post-production')
-allocated_types = ('compute', 'storage')
+allocated_types = ('Compute', 'Storage')
 
 def CiderInfrastructure_AllocatedResources(affiliation='ACCESS', result='OBJECTS'):
-    # Active sub-resources of the desired type
-    child_resources = CiderInfrastructure.objects.filter(
-                                            Q(cider_type__in=allocated_types) &
-                                            Q(latest_status__in=active_statuses) &
-#                                            Q(other_attributes__is_allocated=False) &
-                                            Q(project_affiliation__icontains=affiliation)
-                                               )
-    child_parent_ids = child_resources.values_list('parent_resource', flat=True)
-
-    # Active base resources
-    parent_resources = CiderInfrastructure.objects.filter(
-                                            Q(cider_resource_id__in=child_parent_ids)
-                                                )
+    resources = CiderInfrastructure.objects.filter(
+        Q(cider_type__in=allocated_types) &
+        Q(latest_status__in=active_statuses) &
+        Q(project_affiliation__icontains=affiliation)
+    )
     if result.upper() == 'RESOURCEID':
-        return(list(parent_resources.values_list('info_resourceid', flat=True)))
+        return(list(resources.values_list('info_resourceid', flat=True)))
     else: # Model objects
-        return(parent_resources)
+        return(resources)
             
 # Introduced 2022-04-30 by JP for ACCESS
 def CiderInfrastructure_ActiveOnlineServices(affiliation='ACCESS', result='OBJECTS'):
     # Active sub-resources of the desired type
-    child_resources = CiderInfrastructure.objects.filter(
-                                            Q(cider_type='online_service') &
-                                            Q(latest_status__in=active_statuses) &
-                                            Q(project_affiliation__icontains=affiliation)
-                                               )
-    child_parent_ids = child_resources.values_list('parent_resource', flat=True)
-
-    # Active base resources
-    parent_resources = CiderInfrastructure.objects.filter(
-                                            Q(cider_resource_id__in=child_parent_ids)
-                                                )
+    resources = CiderInfrastructure.objects.filter(
+            Q(cider_type='Online Service') &
+            Q(latest_status__in=active_statuses) &
+            Q(project_affiliation__icontains=affiliation)
+        )
     if result.upper() == 'RESOURCEID':
-        return(list(parent_resources.values_list('info_resourceid', flat=True)))
+        return(list(resources.values_list('info_resourceid', flat=True)))
     else: # Model objects
-        return(parent_resources)
+        return(resources)
