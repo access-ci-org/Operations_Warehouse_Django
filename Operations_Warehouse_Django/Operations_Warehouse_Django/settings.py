eb85aba34f6599016e5213f53b06692da21dee4b
John-Paul Navarro <navarro@anl.gov>
2024-08-25 09:00:43 -0500

    Add info_django to PostgreSQL search_path, where django objects will be rather than public

--- Operations_Warehouse_Django/Operations_Warehouse_Django/settings.py
+++ Operations_Warehouse_Django/Operations_Warehouse_Django/settings.py
@@ -109,7 +109,7 @@ for db in DATABASES:
     DATABASES[db]['ENGINE'] = 'django.db.backends.postgresql'
     DATABASES[db]['PORT'] = os.environ.get('PGPORT', CONF.get('DB_PORT', '5432'))
     DATABASES[db]['CONN_MAX_AGE'] = 600 # Persist DB connections
-    DATABASES[db]['OPTIONS'] = {'options': '-c search_path=info,public'}
+    DATABASES[db]['OPTIONS'] = {'options': '-c search_path=info_django,info,public'}
 
 #DATABASE_ROUTERS = ['xsede_warehouse.router.ModelDatabaseRouter',]
 #from xsede_warehouse.router import *
