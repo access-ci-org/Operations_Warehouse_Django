001006fbdfdd3110676ea2d9b99a1669b031d1db
Eric Blau <blau@anl.gov>
2024-10-28 15:50:49 -0500

    Path converters for software-by-ID routes should be "path" not "str" to allow "/" characters in the ID  CTS-237 ATS-11502

--- Operations_Warehouse_Django/resource_v4/urls.py
+++ Operations_Warehouse_Django/resource_v4/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path(r'v4/catalog_search/', Catalog_Search.as_view(), name='catalog-search'),
-    path(r'v4/catalog/id/<str:id>/', Catalog_Detail.as_view(), name='catalog-detail'),
-    path(r'v4/local/id/<str:id>/', Local_Detail.as_view(), name='local-detail-globalid'),
+    path(r'v4/catalog/id/<path:id>/', Catalog_Detail.as_view(), name='catalog-detail'),
+    path(r'v4/local/id/<path:id>/', Local_Detail.as_view(), name='local-detail-globalid'),
     path(r'v4/local_search/', Local_Search.as_view(), name='local-search'),
 #    path(r'^provider/id/(?P<id>.+)/$',
 #        Provider_Detail.as_view(), name='provider-detail'),
@@ -13,7 +13,7 @@ urlpatterns = [
 ##        cache_page(60 * 60)(Provider_Search.as_view()), name='provider-search'),
 #        Provider_Search.as_view(), name='provider-search'),
     path(r'v4/resource_types/', cache_page(60 * 60)(Resource_Types_List.as_view()), name='resource-types-list'),
-    path(r'v4/resource/id/<str:id>/', Resource_Detail.as_view(), name='resource-detail'),
+    path(r'v4/resource/id/<path:id>/', Resource_Detail.as_view(), name='resource-detail'),
     path(r'v4/resource_search/', Resource_Search.as_view(), name='resource-search'),
     path(r'v4/resource_esearch/', Resource_ESearch.as_view(), name='resource-esearch'),
     path(r'v4/relations_cache/', Relations_Cache.as_view(), name='relations-cache'),
