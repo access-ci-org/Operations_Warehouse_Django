fa4c3ac28ee6c636d3fdb24fe489311129d1929b
Eric Blau <blau@anl.gov>
2025-02-07 11:36:43 -0600

    fix user importing database backup

--- developer/docker/scripts/populatedb.sh
+++ developer/docker/scripts/populatedb.sh
@@ -2,5 +2,5 @@
 
 #docker exec -it $1 psql -U info_django -h localhost -p 5432 warehouse2 -f /opt/app/dbrestore/db.backup
 #docker exec -it $1 psql -U info_django -h localhost -p 5432 warehouse2 -f /opt/app/dbrestore/django.dump.1701118801
-psql -U info_django -h db -p 5432 warehouse2 -f /opt/app/dbrestore/django.dump.latest
+psql -U opsdba -h db -p 5432 warehouse2 -f /opt/app/dbrestore/django.dump.latest
 
