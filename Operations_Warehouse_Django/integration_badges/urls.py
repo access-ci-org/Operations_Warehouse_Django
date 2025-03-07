f60efb033ed8a4e366e9cc65aa6f61ce737bbba0
Dinuka De Silva <l.dinukadesilva@gmail.com>
2025-03-07 11:33:45 -0500

    Refactoring

--- Operations_Warehouse_Django/integration_badges/urls.py
+++ Operations_Warehouse_Django/integration_badges/urls.py
@@ -5,13 +5,13 @@ from integration_badges.views import *
 # Define our custom URLs
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = [
-     path(r'v1/roadmap/', Integration_Roadmap_v1.as_view(), name='integration-roadmap-v1-all'),
+     path(r'v1/roadmaps/', Integration_Roadmap_v1.as_view(), name='integration-roadmap-v1-all'),
      path(r'v1/roadmap/<str:integration_roadmap_id>/', Integration_Roadmap_v1.as_view(), name='integration-roadmap-v1-id'),
-     path(r'v1/badge/', Integration_Badge_v1.as_view(), name='integration-badges-v1-all'),
+     path(r'v1/badges/', Integration_Badge_v1.as_view(), name='integration-badges-v1-all'),
      path(r'v1/badge/<str:integration_badge_id>/', Integration_Badge_v1.as_view(), name='integration-badges-v1-id'),
+     path(r'v1/badge/<str:integration_badge_id>/tasks/', Integration_Task_v1.as_view(), name='integration-tasks-v1-id'),
      path(r'v1/resources/', Integration_Resource_List_v1.as_view(), name='integration-resources-list-v1-all'),
      path(r'v1/resource/<str:cider_resource_id>/', Integration_Resource_v1.as_view(), name='integration-resource-v1-id'),
-     path(r'v1/task/<str:integration_badge_id>/', Integration_Task_v1.as_view(), name='integration-tasks-v1-id'),
      path(r'v1/resource/<str:cider_resource_id>/badge/<str:integration_badge_id>/workflow/<str:badge_workflow_status>',
           Integration_Resource_Badge_Status_v1.as_view(), name='integration-resource-badge-status-v1-id'),
      path(r'v1/resource/<str:cider_resource_id>/badge/<str:integration_badge_id>/task/<str:integration_task_id>/workflow/<str:badge_task_workflow_status>',
