eff346498c61539094a684578a9775b46197f715
Jesse Lambertson (IU) <jelambe@iu.edu>
2025-05-22 13:15:47 -0400

    ctt-289 tweaked requirements.txt, updated README, a couple other tiny things

--- developer/docker/sbin/warehouse_api.sh
+++ developer/docker/sbin/warehouse_api.sh
@@ -8,4 +8,5 @@ export DJANGO_CONFIG=$APP_HOME/conf/$APP_NAME.conf
 #exec $APP_HOME/python/bin/gunicorn --config=$APP_HOME/conf/$APP_NAME.gunicorn.conf.py $APP_SOURCE_NAME.wsgi:application
 APP_DJANGO="/soft/warehouse-2.0/PROD/Operations_Warehouse_Django"
 cd $APP_DJANGO
+# exec python3 manage.py migrate
 exec python3 manage.py runserver 0.0.0.0:8000
