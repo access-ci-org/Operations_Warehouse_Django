2b404e08bdb251f1167e1cf788cd4544afc0d6f5
jyssy <jessel@phantomlandscapes.com>
2025-10-21 09:38:23 -0400

    updating urls for better alighment

--- Operations_Warehouse_Django/integration_views/urls.py
+++ Operations_Warehouse_Django/integration_views/urls.py
@@ -5,8 +5,5 @@ from .views import RoadmapResourceBadgesView, GroupBadgeStatusView
 
 urlpatterns = [
     path(r'v1/resource_pivot/', RoadmapResourceBadgesView.as_view(), name='resource-pivot'),
-<<<<<<< HEAD
-=======
     path(r'v1/group_pivot/', GroupBadgeStatusView.as_view(), name='group-pivot'),
->>>>>>> 5dc18fa (ctt 727 adding rp group view v1)
 ]
