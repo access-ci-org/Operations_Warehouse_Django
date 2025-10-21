921296521544998f06043dd51d5359fd7563c093
jyssy <jessel@phantomlandscapes.com>
2025-10-21 09:13:01 -0400

    ctt 727 adding rp group view v1

--- Operations_Warehouse_Django/integration_views/urls.py
+++ Operations_Warehouse_Django/integration_views/urls.py
@@ -1,7 +1,12 @@
 # roadmap_pivot/urls.py
 from django.urls import path
-from .views import RoadmapResourceBadgesView
+from .views import RoadmapResourceBadgesView, GroupBadgeStatusView
+
 
 urlpatterns = [
     path(r'v1/resource_pivot/', RoadmapResourceBadgesView.as_view(), name='resource-pivot'),
+<<<<<<< HEAD
+=======
+    path(r'v1/group_pivot/', GroupBadgeStatusView.as_view(), name='group-pivot'),
+>>>>>>> 5dc18fa (ctt 727 adding rp group view v1)
 ]
