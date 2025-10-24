7314f7e3e8bfb8d2b17232d74b4e095685b8e97a
ericblau <blau@globus.org>
2025-10-24 13:18:18 -0500

    Ctt 695 implement integration badges update permissions (#61)
    * Add Staff role permissions (concierge, badge maintainer, roadmap maintainer)
    
    * Implement update permissions per https://access-ci.atlassian.net/browse/CTT-694
    Also, reformat using black https://pypi.org/project/black/

--- Operations_Warehouse_Django/integration_badges/permissions.py
+++ Operations_Warehouse_Django/integration_badges/permissions.py
@@ -1,7 +1,7 @@
-import re
 from rest_framework import permissions
 from cider.serializers import *
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
@@ -22,8 +22,9 @@ class ResourceIDError(Exception):
     def __str__(self):
         return f"{self.message}"
 
+
 class IsBadgeRole(permissions.BasePermission):
-    """This is a base class that implements the logic for authz for Badge Roles"""
+    """Base class that implements the logic for authz for Badge Roles"""
     def __init__(self, rolename=''):
         self.rolename = rolename
 
@@ -33,10 +34,11 @@ class IsBadgeRole(permissions.BasePermission):
         info_resourceid = resource.info_resourceid
         try:
             cidergroup = CiderGroups.objects.filter(info_resourceids__contains=[info_resourceid]).first()
-        except:
+        except Exception:
             return False
         if not cidergroup:
-            raise(ResourceIDError(f"{info_resourceid} not found in any CiderGroup"))
+            raise (ResourceIDError(
+                       f"{info_resourceid} not found in any CiderGroup"))
             return False
         else:
             info_groupid = cidergroup.info_groupid
@@ -55,19 +57,12 @@ class IsBadgeRole(permissions.BasePermission):
             return False
 
 
-
-
-class IsRoadmapMaintainer(IsBadgeRole):
-    """This is an example that authorizes all authenticated users"""
-    def __init__(self, rolename):
-        super().__init__(rolename)
-        self.rolename = 'roadmap_maintainer'
-
 class IsImplementer(IsBadgeRole):
     """This is an example that authorizes all authenticated users"""
-    def __init__(self, rolename):
+    def __init__(self, rolename='implementer'):
         super().__init__(rolename)
         self.rolename = 'implementer'
+
 
 class IsCoordinator(IsBadgeRole):
     """This is an example that authorizes all authenticated users"""
@@ -75,27 +70,39 @@ class IsCoordinator(IsBadgeRole):
         super().__init__(rolename)
         self.rolename = 'coordinator'
 
-class IsFooCoordinator(permissions.BasePermission):
-    """This is an example that authorizes all authenticated users"""
+
+class IsStaffRole(permissions.BasePermission):
+    """This is a base class that implements the logic for authz for Staff Roles"""
+    def __init__(self, rolename=''):
+        self.rolename = rolename
+
     def has_permission(self, request, view):
-        info_resourceid = request.parser_context["kwargs"]['info_resourceid']
-        resource = CiderInfrastructure.objects.get(pk=info_resourceid)
-        info_resourceid = resource.info_resourceid
-        cleaned_resourceid = re.sub(r'xsede\.org', 'access-ci.org', info_resourceid)
-        cidergroup = CiderGroups.objects.filter(info_resourceids__contains=[cleaned_resourceid]).first()
-        info_groupid = cidergroup.info_groupid
-        # Do we want different perms here for read only?
-        # Read permissions are allowed to any request.
-        #if request.method in ['GET', 'HEAD', 'OPTIONS']:
-        #if request.method in ['HEAD', 'OPTIONS']:
-        #    return True
 
         # Write permissions are only allowed to the owner.
         if request.user.is_authenticated:
-            print(f'User {request.user.username} is authenticated')
-            print(f'User permissions {request.user.get_all_permissions()}')
-            print(f'info_groupid is  {info_groupid}')
-            print('cider.coordinator'+'_'+info_groupid)
-            return request.user.has_perm('cider.coordinator'+'_'+info_groupid)
+            #print(f'User {request.user.username} is authenticated')
+            #print(f'User permissions {request.user.get_all_permissions()}')
+            return request.user.has_perm('cider.'+self.rolename)
         else:
             return False
+
+class IsConcierge(IsStaffRole):
+    """This authorizes authenticated users who belong to the group:"""
+    """urn:group:access-ci.org:operations.access-ci.org:concierge (for verifying badges)"""
+    def __init__(self, rolename='concierge'):
+        super().__init__(rolename)
+        self.rolename = 'concierge'
+
+class IsRoadmapMaintainer(IsStaffRole):
+    """This authorizes authenticated users who belong to the group:"""
+    """urn:group:access-ci.org:operations.access-ci.org:roadmap.maintainer (for creating and updating roadmap definitions, and including adding and removing badges from a roadmap)"""
+    def __init__(self, rolename='roadmap.maintainer'):
+        super().__init__(rolename)
+        self.rolename = 'roadmap.maintainer'
+
+class IsBadgeMaintainer(IsStaffRole):
+    """This authorizes authenticated users who belong to the group:"""
+    """urn:group:access-ci.org:operations.access-ci.org:badge.maintainer (for maintaining badge information)"""
+    def __init__(self, rolename='badge.maintainer'):
+        super().__init__(rolename)
+        self.rolename = 'badge.maintainer'
