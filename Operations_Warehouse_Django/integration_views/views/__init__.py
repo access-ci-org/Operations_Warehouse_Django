e3a8744321d55efc1aab14dcac26790535c68ccf
jyssy <jessel@phantomlandscapes.com>
2025-10-28 19:50:23 -0400

    updating views and splitting them

---
+++ Operations_Warehouse_Django/integration_views/views/__init__.py
@@ -0,0 +1,11 @@
+from .resource_pivot_views import RoadmapResourceBadgesView
+from .group_pivot_views import GroupBadgeStatusView
+
+"""
+have split the views into separate py files for reasons of human readability and troubleshooting 
+"""
+
+__all__ = [
+    'RoadmapResourceBadgesView',
+    'GroupBadgeStatusView',
+]
