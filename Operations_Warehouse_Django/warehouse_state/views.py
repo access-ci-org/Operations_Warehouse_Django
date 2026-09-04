af0cd3e1fc90c6c2d0bfee6cf1299d255c43bd0d
JP Navarro <jpnavarro@mac.com>
2026-09-04 13:34:28 -0500

    More prety_json and publisher_list sorting improvements

--- Operations_Warehouse_Django/warehouse_state/views.py
+++ Operations_Warehouse_Django/warehouse_state/views.py
@@ -263,13 +263,23 @@ class PublisherInfo_DbList(ListAPIView):
                 objects = PublisherInfo.objects.all()
             except PublisherInfo.DoesNotExist:
                 raise MyAPIException(code=status.HTTP_404_NOT_FOUND, detail='No objects found')
-        try:
-            sort_by = request.GET.get('sort')
-            objects_sorted = objects.order_by(sort_by)
+                sort_by = request.GET.get('sort','-ProcessingTimestamp')
+
+        sort_by = request.GET.get('sort','-CreationTime')
+        try: # Primary and secondary sort
+            if sort_by.endswith('CreationTime'):
+                objects_sorted = objects.order_by(sort_by, 'ResourceID')
+            elif sort_by.endswith('ResourceID'):
+                objects_sorted = objects.order_by(sort_by, '-CreationTime')
+            elif sort_by.endswith('Version'):
+                objects_sorted = objects.order_by(sort_by, '-CreationTime')
+            else:
+                objects_sorted = objects.order_by(sort_by)
         except:
             objects_sorted = objects
+
         serializer = PublisherInfo_DetailURL_DbSerializer(objects_sorted, context={'request': request}, many=True)
-        return MyAPIResponse({'record_list': serializer.data}, template_name='warehouse_state/publisher_list.html')
+        return MyAPIResponse({'record_list': serializer.data, 'sort_by': sort_by}, template_name='warehouse_state/publisher_list.html')
 
 class PublisherInfo_Detail(GenericAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
